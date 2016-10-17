3cab570518ba5c3a8d58b87c479c7f93ef6ec838
Melanie Gershman <melanie.gershman@gmail.com>
2016-10-17 19:50:59 -0400

    Add class attributes and methods

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,27 @@
+require 'pry'
+
 class Apartment
+  attr_accessor :rooms, :monthly_rent
+
+  def initialize(args={})
+    @number = args.fetch(:number, '')
+    @rooms=args.fetch(:rooms, [])
+    @monthly_rent=args.fetch(:monthly_rent, 1000)
+  end
+
+  def total_sqft
+    rooms.reduce(0) {|sum, room| sum + room.sqft}
+  end
+
+  def price_per_sqft
+    monthly_rent / (self.total_sqft).to_f
+  end
+
+  def room_count
+    rooms.length
+  end
+
+  def bedroom_count
+    rooms.count {|room| room.name == "bedroom"}
+  end
 end
--- building.rb
+++ building.rb
@@ -1,2 +1,28 @@
 class Building
+  attr_reader :apartments
+
+  def initialize(args={})
+    @apartments=args.fetch(:apartments, [])
+
+  end
+
+  def total_room_count
+    apartments.reduce(0) {|sum, apartment| sum + apartment.room_count}
+  end
+
+  def total_monthly_revenue
+    apartments.reduce(0) {|sum, apartment| sum + apartment.monthly_rent}
+  end
+
+  def apartments_by_rent
+    apartments.sort {|apt_a, apt_b| apt_b.monthly_rent <=> apt_a.monthly_rent}
+  end
+
+  def find_apartments_by_bedroom_count(num_bedrooms)
+    apartments.find {|apartment| apartment.bedroom_count == num_bedrooms } 
+  end
+
+  def total_sqft
+    apartments.reduce(0) {|sum, apartment| sum + apartment.total_sqft}
+  end
 end
