b9c06bd66b5d02f48005682bc1ae9f954858578c
Marissa Traina <marissatraina@gmail.com>
2016-10-17 18:48:29 -0400

    Pass rspec tests for apartment

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,28 @@
+require 'pry'
+
 class Apartment
+  attr_reader :rooms, :monthly_rent
+
+  def initialize(args = {})
+    @rooms = args.fetch(:rooms, [])
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+  end
+
+  def total_sqft
+    rooms.reduce(0) { |sum, room| sum + room.sqft }
+  end
+
+  def price_per_sqft
+    monthly_rent.to_f/total_sqft.to_f
+  end
+
+  def room_count
+    rooms.length
+  end
+
+  def bedroom_count
+    bedrooms = rooms.select { |room| "bedroom".include? room.name }
+    bedrooms.length
+  end
+
 end
