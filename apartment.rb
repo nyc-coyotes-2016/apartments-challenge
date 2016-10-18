ed154cbb24104da04abc26bfa4552e2b66bb2ef8
Natalie_Liles <Natalieliles@gmail.com>
2016-10-18 00:34:58 -0400

    Add methods.

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,31 @@
+require_relative 'room'
+
 class Apartment
+	attr_reader :monthly_rent, :rooms
+
+	def initialize (args = {})
+		@rooms = args.fetch(:rooms,[])
+		@monthly_rent = args.fetch(:monthly_rent, 1000)
+	end 
+
+	def total_sqft
+		rooms.inject(0) { |sum, room| room.sqft + sum }
+	end 
+
+	def price_per_sqft
+		monthly_rent/total_sqft.to_f
+	end 
+
+	def room_count 
+		rooms.size
+	end 
+
+	def bedroom_count
+		(rooms.select { |room| room == 'bedroom'}).count
+	end
+
 end
+
+
+
+
