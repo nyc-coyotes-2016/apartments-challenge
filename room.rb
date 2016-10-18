0d4c253c1fa1548b42e17a4546b8e12ee8790ba6
Matt C <email": null>
2016-10-18 10:35:16 -0400

    Add methods to pass all rspec tests.

--- room.rb
+++ room.rb
@@ -1,2 +1,13 @@
 class Room
+
+  attr_reader :sqft
+  attr_accessor :name
+
+  def initialize(args = {})
+    @name = args.fetch(:name, 'bedroom')
+    @sqft = args.fetch(:sqft, 120)
+    @windows = args.fetch(:windows) { rand(0..2) }
+  end
+
+
 end
