5ac8326b28907da4901faaa69f52423d47238e62
Gregory V. Bilello <gregory.bilello@gmail.com>
2016-10-17 22:45:28 -0400

    Define room, apartment, and building classes with appropriate instance variables and methods - all rspec tests pass

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,16 @@
 class Apartment
+
+	attr_reader :number, :rooms, :monthly_rent, :total_sqft, :bedroom_count, :room_count, :price_per_sqft
+
+	def initialize(args = {})
+		@number = args.fetch(:number, 0)
+		@rooms = args.fetch(:rooms, Array.new)
+		@monthly_rent = args.fetch(:monthly_rent, 1000)
+
+		@total_sqft = rooms.inject(0) { |total, room| total + room.sqft }
+		@price_per_sqft = @monthly_rent / @total_sqft.to_f
+		@room_count = rooms.count
+		@bedroom_count = rooms.count { |room| room.name == 'bedroom' }
+	end
+
 end
--- building.rb
+++ building.rb
@@ -1,2 +1,26 @@
 class Building
+
+	attr_reader :address
+	attr_accessor :apartments, :total_room_count, :total_monthly_revenue, :total_sqft
+
+	def initialize(args = {})
+		@address = args.fetch(:address, "")
+		@apartments = args.fetch(:apartments, Array.new)
+
+		@total_room_count = apartments.inject(0) { |total, apartment| total + apartment.room_count }
+		@total_monthly_revenue = apartments.inject(0) { |total, apartment| total + apartment.monthly_rent }
+		@total_sqft = apartments.inject(0) { |total, apartment| total + apartment.total_sqft }
+	end
+
+	def apartments_by_rent
+		# negative in front of apartments.monthly_rent reverses
+		# the standard Ruby sort order from ascending, to
+		# descending
+		apartments.sort_by { |apartment| -apartment.monthly_rent }
+	end
+
+	def find_apartments_by_bedroom_count(num_of_bedrooms)
+		apartments.find { |apartment| apartment.bedroom_count == num_of_bedrooms }
+	end
+
 end
