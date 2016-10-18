de218b2cd18a602374dc664275b24ad7632786c9
Miles McArdle-Coe <email": null>
2016-10-18 10:44:51 -0400

    complete challenge

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,19 @@
 class Apartment
+  attr_reader :rooms , :monthly_rent
+  def initialize(args={})
+    @rooms = args.fetch(:rooms,[])
+    @monthly_rent = args.fetch(:monthly_rent,1000)
+  end
+  def total_sqft
+    rooms.inject(0) {|sum,room| sum + room.sqft}
+  end
+  def price_per_sqft
+    monthly_rent / total_sqft.to_f
+  end
+  def room_count
+    rooms.size
+  end
+  def bedroom_count
+    rooms.select {|room| room.name == "bedroom"}.size
+  end
 end
--- building.rb
+++ building.rb
@@ -1,2 +1,21 @@
 class Building
+  attr_reader :apartments
+  def initialize(args={})
+    @apartments = args.fetch(:apartments,[])
+  end
+  def total_room_count
+    apartments.inject(0) {|sum,apartment| sum + apartment.room_count}
+  end
+  def total_monthly_revenue
+    apartments.inject(0) {|sum,apartment| sum + apartment.monthly_rent}
+  end
+  def apartments_by_rent
+    apartments.sort {|apartment2,apartment1| apartment1.monthly_rent <=> apartment2.monthly_rent}
+  end
+  def find_apartments_by_bedroom_count(num)
+    apartments.find {|apartment| apartment.bedroom_count == num }
+  end
+  def total_sqft
+    apartments.inject(0) {|sum,apartment| sum + apartment.total_sqft}
+  end
 end
