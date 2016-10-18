42adc1291e9c046d4138cd6d8f1ded768268c1c9
Arshdeep Singh <singh_arsy@yahoo.com>
2016-10-18 15:00:10 -0400

    minor changes to the files

--- building.rb
+++ building.rb
@@ -1,38 +1,39 @@
 class Building
-	attr_reader :apartments
+	attr_reader :apartments, :address
 
-		def initialize(args={})
-			@apartments = args.fetch(:apartments, [])
-		end
+	def initialize(args={})
+		@apartments = args.fetch(:apartments, [])
+		@address = args.fetch(:address, "")
+	end
 
 
-	  def total_room_count
+	def total_room_count
 	    apartments.inject(0) do |total, apartment|
-	      total + apartment.room_count
+	    total + apartment.room_count
 	    end
-	  end
+	end
 
-	  def total_monthly_revenue
+	def total_monthly_revenue
 	    apartments.inject(0) do |total, rent|
-	      total + rent.monthly_rent
+	    total + rent.monthly_rent
 	    end
-	  end
+	end
 
-	  def apartments_by_rent
+	def apartments_by_rent
 	    apartments.sort do |apt1, apt2|
-	      apt2.monthly_rent <=> apt1.monthly_rent
+	    apt2.monthly_rent <=> apt1.monthly_rent
 	    end
-	  end
+	end
 
-	  def find_apartments_by_bedroom_count(num)
+	def find_apartments_by_bedroom_count(num)
 	    apartments.find do |apartment|
-	      apartment.bedroom_count == num
+	    apartment.bedroom_count == num
 	    end
-	  end
+	end
 
-	  def total_sqft
+	def total_sqft
 	    apartments.inject(0) do |total, apartment|
-	      total + apartment.total_sqft
+	    total + apartment.total_sqft
 	    end
-	  end
+	end
 end
